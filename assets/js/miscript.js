1040fae382cd46e3dec9fe9a44ba538d51469773
24orelllana24 <24orellana24@gmail.com>
2021-10-12 20:12:28 -0300

    Cometarios al código js

--- assets/js/miscript.js
+++ assets/js/miscript.js
@@ -1,5 +1,7 @@
+// Declaración de variables
 let numMult, contador = 0;
 
+// Ciclo para ingresar el número condicionado a que sea entre 1 y 20
 do {
   if (contador > 0) {
     alert(`¡¡¡Número ingresado fuera de rango!!!\nIngrese un número entero entre 1 y 20`);
@@ -8,11 +10,13 @@ do {
   numMult = parseInt(prompt('Ingrerse número, del cual necesita su tabla de multiplicar: '));
 } while (isNaN(numMult) || numMult < 1 || numMult > 20);
 
+// Ciclo for para mostrar la tabla de multiplicar por consola
 for ( let i = 1; i <= numMult; i++) {
   let resultado = i * numMult;
   console.log(`${numMult} x ${i} = ${resultado}`);
 }
 
+// Función arrow para calcular el factorial del número ingresado
 const miFactorial = numero => {
   let numFact = 1;
   for (let i = 1; i <= numMult; i++) {
@@ -21,4 +25,5 @@ const miFactorial = numero => {
   return numFact;
 }
 
+// Mostrar por consola el factorial calculado en la función
 console.log(`El factorial del número ${numMult}! es: ${miFactorial(numMult)}`);
