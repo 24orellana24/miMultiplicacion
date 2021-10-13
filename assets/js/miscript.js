c6a576f731a218ead302d3357b56937cd878b544
24orelllana24 <24orellana24@gmail.com>
2021-10-12 21:25:38 -0300

    corrección mostrar factorial

--- assets/js/miscript.js
+++ assets/js/miscript.js
@@ -10,20 +10,23 @@ do {
   numMult = parseInt(prompt('Ingrerse número, del cual necesita su tabla de multiplicar: '));
 } while (isNaN(numMult) || numMult < 1 || numMult > 20);
 
-// Ciclo for para mostrar la tabla de multiplicar por consola
-for ( let i = 1; i <= numMult; i++) {
-  let resultado = i * numMult;
-  console.log(`${numMult} x ${i} = ${resultado}`);
+// Función arrow con ciclo for para mostrar la tabla de multiplicar
+const miMultiplicacion = numero => {
+  for ( let i = 1; i <= numMult; i++) {
+    let resultado = i * numMult;
+    console.log(`${numMult} x ${i} = ${resultado}`);
+  }
 }
 
-// Función arrow para calcular el factorial del número ingresado
+// Función arrow con ciclo for para mostrar el factorial del número ingresado
 const miFactorial = numero => {
   let numFact = 1;
   for (let i = 1; i <= numMult; i++) {
     numFact *= i;
+    console.log(`El factorial del número ${i} es ${numFact}`);
   }
-  return numFact;
 }
 
-// Mostrar por consola el factorial calculado en la función
-console.log(`El factorial del número ${numMult}! es: ${miFactorial(numMult)}`);
+// Llamada de mis funciones para que se ejecuten y muestren el resultado por consola
+miMultiplicacion();
+miFactorial();
